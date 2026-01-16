5c87a7726b537d6576b4d753c7820a8d76effa67
mohammedfalaah <falahmkba@gmail.com>
2026-01-16 12:45:18 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from 'react-hot-toast';
 
-const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || '1020234324057-4l2u2ctn6jtq2sgolg569u3mf3aqd9pm.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID ='1020234324057-4l2u2ctn6jtq2sgolg569u3mf3aqd9pm.apps.googleusercontent.com';
 
 console.log("Google Client ID:", GOOGLE_CLIENT_ID);
 
