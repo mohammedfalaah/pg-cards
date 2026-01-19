f7343a4adc7c8cab2b1ab335a49dcf0cc1fa68b7
mohammedfalaah <falahmkba@gmail.com>
2026-01-19 11:44:29 +0530

    google

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from 'react-hot-toast';
 
-const GOOGLE_CLIENT_ID ='1020234324057-4l2u2ctn6jtq2sgolg569u3mf3aqd9pm.apps.googleusercontent.com';
+const REACT_APP_GOOGLE_CLIENT_ID ='1020234324057-4l2u2ctn6jtq2sgolg569u3mf3aqd9pm.apps.googleusercontent.com';
 
-console.log("Google Client ID:", GOOGLE_CLIENT_ID);
-console.log("Google Client ID:", GOOGLE_CLIENT_ID);
+console.log("Google Client ID:", REACT_APP_GOOGLE_CLIENT_ID);
+console.log("Google Client ID:", REACT_APP_GOOGLE_CLIENT_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={REACT_APP_GOOGLE_CLIENT_ID}>
       <App />
       <Toaster
   position="top-center"
